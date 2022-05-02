e654c032ae823018c2b762a339cecc4b70de3281
KJH <stonehead334@gmail.com>
2022-05-02 17:54:25 +0900

    Style Part-1

--- App.js
+++ App.js
@@ -3,27 +3,50 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 //flexbox로 부모를 만들고 자식들에게 비율을 조정할수 있다
 
-
 //모든 텍스트는 TEXT안에 들어가야 된다 웹이 아니기 때문에
 const styles = StyleSheet.create({
   container: {
-    flex: 0.5,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "tomato",
   },
-  text: {
-    fontSize: 20,
-    color: "tomato",
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   },
 });
 
